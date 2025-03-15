1a6f956dbf542a8c836b8798f0d6809365c37ea0
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-03-15 12:49:56 +0530

    changes in server.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,14 +2,9 @@ import "./config/instrument.js"
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import * as Sentry from "@sentry/node";
-// import { clerkWebhooks } from "./controller/webhooks.controller.js";
-// import { clerkMiddleware } from '@clerk/express'
-
 
 // Initialize express
 const app = express();
-Sentry.setupExpressErrorHandler(app);
 
 // Middlewares
 app.use(cors({
@@ -18,23 +13,9 @@ app.use(cors({
     }
 ));
 app.use(express.json());
-// Middleware to parse raw body for Clerk webhooks
-// app.use('/webhooks', express.json({
-//     verify: (req, res, buf) => { req.rawBody = buf.toString(); }
-// }));
-// app.use(clerkMiddleware());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-// import Routes
-app.get('/', (req,res) => {
-    res.send("Hello World")
-})
-app.get("/debug-sentry",(req,res) => {
-    throw new Error("Test error");
-})
-// app.post('/webhooks',clerkWebhooks)
 import companyRoutes from "./routes//company.routes.js"
 import jobRoutes from './routes/job.routes.js'
 import userRoutes from './routes/user.routes.js'
@@ -45,8 +26,5 @@ app.use('/api/company',companyRoutes)
 app.use('/api/jobs',jobRoutes)
 app.use('/api/users',userRoutes)
 app.use('/api/chatbot',chatbotRoutes)
-
-
-
 
 export { app }
--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,26 @@ import connectCloudinary from './config/cloudinary.js';
 
 dotenv.config();
 
+// const startServer = async () => {
+//   try {
+//     await connectDB();
+//     console.log("✅ MongoDB Connected");
+
+//     await connectCloudinary();
+//     console.log("✅ Cloudinary Connected");
+
+//     if (process.env.NODE_ENV !== "production") {
+//       const port = process.env.PORT ;
+//       app.listen(port, "0.0.0.0",() => {
+//         console.log(`🚀 Server running on http://localhost:${port}`);
+//       });
+//     }
+//   } catch (error) {
+//     console.error("❌ Error starting the server:", error);
+//     process.exit(1); // Exit process on failure
+//   }
+// };
+
 const startServer = async () => {
   try {
     await connectDB();
@@ -13,12 +33,10 @@ const startServer = async () => {
     await connectCloudinary();
     console.log("✅ Cloudinary Connected");
 
-    if (process.env.NODE_ENV !== "production") {
-      const port = process.env.PORT ;
-      app.listen(port, "0.0.0.0",() => {
-        console.log(`🚀 Server running on http://localhost:${port}`);
-      });
-    }
+    const port = process.env.PORT || 3000; // Fallback to 3000 if PORT is not set
+    app.listen(port, "0.0.0.0", () => {
+      console.log(`🚀 Server running on http://localhost:${port}`);
+    });
   } catch (error) {
     console.error("❌ Error starting the server:", error);
     process.exit(1); // Exit process on failure
