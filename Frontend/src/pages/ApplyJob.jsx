1fc6ba2c5787a135e65ec2d82ed9f17e7a36ab1e
Padma-Lochan-Sahoo <padmalochansahoo8503@gmail.com>
2025-03-16 11:22:33 +0530

    Apply for job

--- Frontend/src/pages/ApplyJob.jsx
+++ Frontend/src/pages/ApplyJob.jsx
@@ -109,7 +109,7 @@ const ApplyJob = () => {
             </div>
               {/* apply Now button and timeing of job posts */}
               <div className='flex flex-col justify-center text-end text-sm max-md:mx-auto max-md:text-center'>
-                <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded'>{isAlreadyApplied?`Already Applied`:`Apply Now`}</button>
+                <button onClick={applyHandler} className='bg-blue-600 p-2.5 px-10 text-white rounded'>{isAlreadyApplied?`${className="opacity-50 cursor-not-allowed"}Already Applied`:`Apply Now`}</button>
                 <p className='mt-1 text-gray-600'>Posted {moment(jobData.data).fromNow()}</p>
               </div>
 
